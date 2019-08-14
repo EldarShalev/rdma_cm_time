8f005669d918987aa1689855fd909e6cb7ab713f
Eldar Shalev <eldarsh@r-arch-host04.mtr.labs.mlnx>
2019-08-14 15:19:42 +0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/rdma_cm_time.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rdma_cm_time.dir/main.c.o"
+  "rdma_cm_time.pdb"
+  "rdma_cm_time"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rdma_cm_time.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rdma_cm_time.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rdma_cm_time.
+# This may be replaced when dependencies are built.
